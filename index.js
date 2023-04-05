cd151b759409b7f9990d8eb70b0f77c76a87bcef
DanielJDufour <daniel.j.dufour@gmail.com>
2023-04-04 23:14:17 -0400

    added index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+export { default as Envelope } from "./geometry/Envelope.js";
+export { default as Geometry } from "./geometry/Geometry.js";
+export { default as LineString } from "./geometry/LineString.js";
+export { default as Polygon } from "./geometry/Polygon.js";
+export { default as Point } from "./geometry/Point.js";
+export { default as findGeometries } from "./find/geometries.js";
